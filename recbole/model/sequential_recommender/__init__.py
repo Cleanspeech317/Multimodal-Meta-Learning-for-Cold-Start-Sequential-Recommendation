59b10e50b0053b3fd60f1e04b058f08f0854cca5
chenyushuo <297086016@qq.com>
2022-01-11 20:37:04 +0800

    FEA: add sasrecfeat.py

--- recbole/model/sequential_recommender/__init__.py
+++ recbole/model/sequential_recommender/__init__.py
@@ -18,4 +18,4 @@ from recbole.model.sequential_recommender.sasrecf import SASRecF
 from recbole.model.sequential_recommender.srgnn import SRGNN
 from recbole.model.sequential_recommender.stamp import STAMP
 from recbole.model.sequential_recommender.transrec import TransRec
-from recbole.model.sequential_recommender.sasrec_feat import SASRecFeat
+from recbole.model.sequential_recommender.sasrecfeat import SASRecFeat
