7e6aa84aaa5b00d9bcea67c29313504f99a5875f
chenyushuo <297086016@qq.com>
2022-02-23 10:09:31 +0800

    UPD: remove useless file.

--- script/backup/print11-meta-without-IEG-fusion.sh
+++
@@ -1,8 +0,0 @@
-city_list="shanghai hangzhou changsha lanzhou"
-echo "          R@5    R@10   R@20    M@5    M@10   M@20    N@5    N@10   N@20"
-for city in $city_list
-do
-    printf "%-10s" $city
-    tail output/$city/0207-exp/up/down/attention-fusion.meta-test.without-IEG.log -n 2 | head -n 1 | sed -E 's/\)|\]|\}//g' | awk -F',' '{ printf("%.4f %.4f %.4f  %.4f %.4f %.4f  %.4f %.4f %.4f\n", $2, $4, $6, $8, $10, $12, $14, $16, $18) }'
-done
-
